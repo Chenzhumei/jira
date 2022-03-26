865be4bec97a04dbc9362bb7f94b08685114ab5e
Chenzhumei <zcm_1018@163.com>
2022-03-26 15:05:16 +0800

    使用useAuth切换登录和非登录状态

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
-import { LoginScreen } from './screens/login';
-import { ProjectListScreen } from './screens/project-list';
+import { AuthenticatedApp } from './authenticatedApp';
+import { useAuth } from './context/auth-context';
+import UnauthenticatedApp from './unauthenticated-app';
 
 function App() {
+  const {user} = useAuth();
   return (
     <div className="App">
-      <LoginScreen/>
-      {/* <ProjectListScreen /> */}
+      {user ? <AuthenticatedApp /> : <UnauthenticatedApp/>}
     </div>
   );
 }
