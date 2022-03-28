0d27c4669ea9fde1215104c0073ba12d98f6734e
Chenzhumei <zcm_1018@163.com>
2022-03-28 15:04:54 +0800

    引入antd

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { DevTools, loadServer } from "jira-dev-tool";
+import 'antd/dist/antd.css';
 import { AppProviders } from './context';
 
 loadServer(() => ReactDOM.render(
--- src/unauthenticated-app/index.tsx
+++ src/unauthenticated-app/index.tsx
@@ -1,13 +1,16 @@
+import { Button, Card } from "antd";
 import React, { useState } from "react";
 import { LoginScreen } from "./login";
 import { RegisterScreen } from "./register";
 
 export default function UnauthenticatedApp() { 
     const [isRegister, setIsRegister] = useState(false);
-    return <div>
-        {
-            isRegister ? <RegisterScreen/> : <LoginScreen/>
-        }
-        <button onClick={() => setIsRegister(!isRegister)}>切换到{isRegister ? '登录' : '注册'}</button>
+    return <div style={{display:'flex', justifyContent:'center'}}>
+        <Card>
+            {
+                isRegister ? <RegisterScreen/> : <LoginScreen/>
+            }
+            <Button type='primary' onClick={() => setIsRegister(!isRegister)}>切换到{isRegister ? '登录' : '注册'}</Button>
+        </Card>
     </div>
 }
